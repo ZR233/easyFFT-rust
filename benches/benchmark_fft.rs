cca9d9e31ce279de38de7e7a99b63dab7691b5b5
zr <zhourui818@126.com>
2022-06-30 20:57:08 +0800

    init

--- benches/benchmark_fft.rs
+++ benches/benchmark_fft.rs
@@ -27,8 +27,8 @@ fn create_sample(shape: Vec<i32>,
 
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let shape = vec![512];
-    let batches = 10000;
+    let shape = vec![1024, 1024];
+    let batches = 10;
     let sample = create_sample(shape.clone(), batches);
 
     let mut plan_gpu = Plan::new_complex_float(
